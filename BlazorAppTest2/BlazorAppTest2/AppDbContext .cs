c6591be49e68301e1f9474869a294c107841bb0d
raju <raju@TISWIN3.ASTACUSVIZAG.local>
2025-09-05 13:16:54 +0530

    Update code

---
+++ BlazorAppTest2/BlazorAppTest2/AppDbContext .cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAppTest2
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; } = default!;
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
--- BlazorAppTest2/BlazorAppTest2/Program.cs
+++ BlazorAppTest2/BlazorAppTest2/Program.cs
@@ -1,4 +1,24 @@
-using BlazorAppTest2.Components;
+//using BlazorAppTest2.Data;
+using BlazorAppTest2;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddServerSideBlazor();
+builder.Services.AddRazorPages();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+app.MapBlazorHub();
+app.MapFallbackToPage("/Home");//_Host
+app.Run();
+
+
+
+/*using BlazorAppTest2.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,4 +44,4 @@ app.UseAntiforgery();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
-app.Run();
+app.Run();*/
